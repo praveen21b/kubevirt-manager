65628f53f70ebf9b6c966e4f86bd28c43b35dbd0
Praveen Hosamani <praveen.h397@gmail.com>
2026-03-03 16:39:38 +0100

    ~ updates to pass tests

--- src/app/components/snapshot-list/snapshot-list.component.spec.ts
+++ src/app/components/snapshot-list/snapshot-list.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DataTablesModule } from 'angular-datatables';
 import { SnapshotListComponent } from './snapshot-list.component';
 
 describe('SnapshotListComponent', () => {
@@ -8,16 +11,36 @@ describe('SnapshotListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SnapshotListComponent]
-    })
-    .compileComponents();
+      declarations: [ SnapshotListComponent ],
+      imports: [ RouterTestingModule, DataTablesModule ],
+      providers: [ provideHttpClient(), provideHttpClientTesting() ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SnapshotListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create', () => { expect(component).toBeTruthy(); });
+  it('should contain component title', () => {
+    expect(fixture.nativeElement.querySelector('h3.card-title')?.textContent).toContain('VM Snapshots');
+  });
+  it('should contain Refresh item', () => {
+    expect(fixture.nativeElement.querySelector('button[title="Refresh..."]')).toBeTruthy();
+  });
+  it('should contain New Snapshot item', () => {
+    expect(fixture.nativeElement.querySelector('button[title="New Snapshot..."]')).toBeTruthy();
+  });
+  it('should contain Main Datatable', () => {
+    expect(fixture.nativeElement.querySelector('#snapshotList_datatable')).toBeTruthy();
+  });
+  it('should contain Window: Snapshot Info', () => {
+    expect(fixture.nativeElement.querySelector('#modal-info')).toBeTruthy();
+  });
+  it('should contain Window: New Snapshot', () => {
+    expect(fixture.nativeElement.querySelector('#modal-new')).toBeTruthy();
+  });
+  it('should contain Window: Delete Snapshot', () => {
+    expect(fixture.nativeElement.querySelector('#modal-delete')).toBeTruthy();
   });
 });
--- src/app/components/snapshot-restore-list/snapshot-restore-list.component.spec.ts
+++ src/app/components/snapshot-restore-list/snapshot-restore-list.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DataTablesModule } from 'angular-datatables';
 import { SnapshotRestoreListComponent } from './snapshot-restore-list.component';
 
 describe('SnapshotRestoreListComponent', () => {
@@ -8,16 +11,36 @@ describe('SnapshotRestoreListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SnapshotRestoreListComponent]
-    })
-    .compileComponents();
+      declarations: [ SnapshotRestoreListComponent ],
+      imports: [ RouterTestingModule, DataTablesModule ],
+      providers: [ provideHttpClient(), provideHttpClientTesting() ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SnapshotRestoreListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create', () => { expect(component).toBeTruthy(); });
+  it('should contain component title', () => {
+    expect(fixture.nativeElement.querySelector('h3.card-title')?.textContent).toContain('VM Restores');
+  });
+  it('should contain Refresh item', () => {
+    expect(fixture.nativeElement.querySelector('button[title="Refresh..."]')).toBeTruthy();
+  });
+  it('should contain New Restore item', () => {
+    expect(fixture.nativeElement.querySelector('button[title="New Restore..."]')).toBeTruthy();
+  });
+  it('should contain Main Datatable', () => {
+    expect(fixture.nativeElement.querySelector('#restoreList_datatable')).toBeTruthy();
+  });
+  it('should contain Window: Restore Info', () => {
+    expect(fixture.nativeElement.querySelector('#modal-info')).toBeTruthy();
+  });
+  it('should contain Window: New Restore', () => {
+    expect(fixture.nativeElement.querySelector('#modal-new')).toBeTruthy();
+  });
+  it('should contain Window: Delete Restore', () => {
+    expect(fixture.nativeElement.querySelector('#modal-delete')).toBeTruthy();
   });
 });
